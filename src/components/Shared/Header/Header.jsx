0ba176c4dc3062a38b373f38d66fc1bc8704fe1e
akanshaagnihotri <shubhiakansha96@gmail.com>
2019-09-04 04:12:27 +0530

    Navbar and hero

--- src/components/Shared/Header/Header.jsx
+++ src/components/Shared/Header/Header.jsx
@@ -4,8 +4,20 @@ import { Button } from "../index";
 
 const Header = () => (
   <div className={css.container}>
-    <h1>Hello</h1>
-    <Button />
+    <div className={css.labelContainer} />
+    <div className={css.linksContainer}>
+      <div className={css.linkWrapper}>
+        <a href={"www.google.com"} className={css.link}>
+          About
+        </a>
+      </div>
+      <div className={css.linkWrapper}>
+        <a href={"www.google.com"} className={css.link}>
+          FAQ
+        </a>
+      </div>
+      <Button className={css.linkWrapper} />
+    </div>
   </div>
 );
 
