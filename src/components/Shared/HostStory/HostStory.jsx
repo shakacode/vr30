6673bf09bcf9abc5640431b319fd465c10b40970
akanshaagnihotri <shubhiakansha96@gmail.com>
2019-09-04 06:36:18 +0530

    Host Images

--- src/components/Shared/HostStory/HostStory.jsx
+++ src/components/Shared/HostStory/HostStory.jsx
@@ -6,7 +6,22 @@ const HostStory = () => (
   <div className={css.container}>
     <div className={css.title}>Richard's Story</div>
     <div className={css.innerContainer}>
-      <div className={css.imagesContainer} />
+      <div className={css.imagesContainer}>
+        <img
+          src="https://www.w3schools.com/html/pic_trulli.jpg"
+          alt="Trulli"
+          width="500"
+          height="383"
+          className={css.primaryImage}
+        />
+        <img
+          src="https://images.pexels.com/photos/2147486/pexels-photo-2147486.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500"
+          alt="Trulli"
+          width="220"
+          height="220"
+          className={css.secondaryImage}
+        />
+      </div>
       <div className={css.description}>
         Richard Sterman has been building VR30.rent since 2013 with the idea of
         creating a master directory of all the legal,
@@ -14,13 +29,20 @@ const HostStory = () => (
         easily find right 30-day rentals for their visits. The lack of
         enforcement on Oahu resulted in no interest in legal rentals. The big
         listing sites like VRBO.com and Airbnb.com hosted the majority of the
-        listings. On August 1st, 2019, Bill 89 passed and became law. Oahu hosts
-        had to switch to 30 minimum stays, but the big listings sites don't
-        attract many visitors interested in doing 30+ day stays. The owners and
-        managers need an alternative marketing source. Richard gets back into
-        high gear with building VR30.rent, but realizes that it's tough to build
-        good technology. Common friends introduce Richard to Justin Gordon who
-        founded{" "}
+        listings.
+        <br />
+        <br />
+        On August 1st, 2019, Bill 89 passed and became law. Oahu hosts had to
+        switch to 30 minimum stays, but the big listings sites don't attract
+        many visitors interested in doing 30+ day stays. The owners and managers
+        need an alternative marketing source.
+        <br />
+        <br />
+        Richard gets back into high gear with building VR30.rent, but realizes
+        that it's tough to build good technology.
+        <br />
+        <br />
+        Common friends introduce Richard to Justin Gordon who founded{" "}
         <a href={"www.shakacode.com"} className={css.link}>
           ShakaCode.com
         </a>
@@ -29,10 +51,14 @@ const HostStory = () => (
         <a className={css.link} href={"www.hawaiichee.com"}>
           HawaiiChee.com
         </a>
-        , since 2015. It immediately became clear to both of them that they
-        should partner up! So Richard has joined Hawaii Chee as the “Manager of
-        Oahu Operations,” responsible for building the Oahu and overall Hawaii
-        inventory for Hawaii Chee (and VR30.rent).
+        , since 2015.
+        <br />
+        <br />
+        It immediately became clear to both of them that they should partner up!
+        So Richard has joined Hawaii Chee as the “Manager of Oahu Operations,”
+        responsible for building the Oahu and overall Hawaii inventory for
+        Hawaii Chee (and VR30.rent).
+        <br />
         <Button className={css.button} />
       </div>
     </div>
